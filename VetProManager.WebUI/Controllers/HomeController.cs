1a024a4cb1a950d99c3df1288ab39f744938e128
Oğuz <burakoguzdegirmenci@gmail.com>
2024-01-07 17:46:40 +0300

    UI katmanı gerekli cssler eklendi.

--- VetProManager.WebUI/Controllers/HomeController.cs
+++ VetProManager.WebUI/Controllers/HomeController.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using VetProManager.WebUI.Models;
-
 namespace VetProManager.WebUI.Controllers {
     public class HomeController : Controller {
         private readonly ILogger<HomeController> _logger;
@@ -12,15 +10,6 @@ namespace VetProManager.WebUI.Controllers {
 
         public IActionResult Index() {
             return View();
-        }
-
-        public IActionResult Privacy() {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error() {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- VetProManager.WebUI/Models/ErrorViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace VetProManager.WebUI.Models {
-    public class ErrorViewModel {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
